f530eaace1b2b8e90c43a7a6459f9dbee352aa7d
PinguwuNoot <hanjasn@gmail.com>
2021-03-05 18:27:43 -0800

    optimization of model classes

--- src/game/console_ui.py
+++ src/game/console_ui.py
@@ -1,40 +1,49 @@
-from model import Board
+from model import *
 from errors import NonEmptyPositionError
+
 
 class UI:
     def __init__(self):
         self.board = Board()
+        self.turn = Mark()
+        self.turn.set_x()  # game starts with X
         self.run_game()
 
+    def reset_game(self):
+        self.board.reset_board()
+        self.turn.set_x()
+
     def run_game(self):
-        turn = self.board.x  # game starts with X
-        while True:
+        run = True
+        while run:
             game_over = self.board.game_won() or self.board.board_full()
             try:
                 print(self.board)
 
                 if game_over:
                     if self.board.game_won():
-                        print(str(self.board.winner) + " wins!")
+                        print(self.board.winner, "wins!")
                     else:
                         print("Game is a tie.")
                     print("Restart? (y/n)", end=" ")
                     if input().lower() == "y":
-                        main()
+                        self.reset_game()
+                        continue
                     else:
+                        # run = False
                         break
                 else:
-                    print(str(turn) + "'s turn")
-                    print("Input position: ", end="") # 9 squares from left to right, top to bottom
+                    print(str(self.turn) + "'s turn")
+                    print("Input position: ", end="")  # 9 squares from left to right, top to bottom
                     position = int(input())
                     if position < 1 or position > 9:
                         raise IndexError
-                    if (turn == self.board.x):
+                    if (self.turn.is_x()):
                         self.board.place_x(position)
-                        turn = self.board.o
+                        self.turn.set_o()
                     else:
                         self.board.place_o(position)
-                        turn = self.board.x
+                        self.turn.set_x()
             except NonEmptyPositionError:
                 print("Position is already filled")
                 continue
@@ -45,8 +54,10 @@ class UI:
                 print("Integer must be from 1 to 9")
                 continue
 
+
 def main():
     UI()
+
 
 if __name__ == "__main__":
     main()
--- src/model.py
+++ src/model.py
@@ -1,23 +1,38 @@
 from errors import NonEmptyPositionError
 
+
 class Mark:
+    X = "X"
+    O = "O"
+    EMPTY = " "
+
     def __init__(self):
         self.set_empty()
 
     def set_x(self):
-        self.mark = "X"
+        self.mark = Mark.X
 
     def set_o(self):
-        self.mark = "O"
+        self.mark = Mark.O
 
     def set_empty(self):
-        self.mark = " "
+        self.mark = Mark.EMPTY
+
+    def is_x(self):
+        return self.mark == Mark.X
+
+    def is_o(self):
+        return self.mark == Mark.O
+
+    def is_empty(self):
+        return self.mark == Mark.EMPTY
 
     def __eq__(self, other):
         return self.mark == other.mark
 
     def __str__(self):
         return self.mark
+
 
 class Board:
     def __init__(self):
@@ -26,23 +41,23 @@ class Board:
             4: Mark(), 5: Mark(), 6: Mark(),
             7: Mark(), 8: Mark(), 9: Mark()
         }
-        self.x = Mark()
-        self.o = Mark()
         self.winner = Mark()
+        self.winner.set_empty()
 
-        self.x.set_x()
-        self.o.set_o()
+    def reset_board(self):
+        for mark in self.board_dict.values():
+            mark.set_empty()
         self.winner.set_empty()
 
     def place_x(self, position):
         if self.board_dict[position] != Mark():
             raise NonEmptyPositionError
-        self.board_dict[position] = self.x
+        self.board_dict[position].set_x()
 
     def place_o(self, position):
         if self.board_dict[position] != Mark():
             raise NonEmptyPositionError
-        self.board_dict[position] = self.o
+        self.board_dict[position].set_o()
 
     # def place_mark(self, position, player):
     #     if player == self.x:
@@ -51,34 +66,33 @@ class Board:
     #         self.place_o(position)
 
     def board_full(self):
-        for square in self.board_dict.values():
-            if square == Mark():
+        for mark in self.board_dict.values():
+            if mark.is_empty():
                 return False
         return True
 
     def game_won(self):
         if self.board_dict[4] == self.board_dict[5] == self.board_dict[6] != Mark() or \
-           self.board_dict[2] == self.board_dict[5] == self.board_dict[8] != Mark() or \
-           self.board_dict[1] == self.board_dict[5] == self.board_dict[9] != Mark() or \
-           self.board_dict[3] == self.board_dict[5] == self.board_dict[7] != Mark():
+                self.board_dict[2] == self.board_dict[5] == self.board_dict[8] != Mark() or \
+                self.board_dict[1] == self.board_dict[5] == self.board_dict[9] != Mark() or \
+                self.board_dict[3] == self.board_dict[5] == self.board_dict[7] != Mark():
             self.winner = self.board_dict[5]
             return True
         elif self.board_dict[1] == self.board_dict[2] == self.board_dict[3] != Mark() or \
-             self.board_dict[1] == self.board_dict[4] == self.board_dict[7] != Mark():
+                self.board_dict[1] == self.board_dict[4] == self.board_dict[7] != Mark():
             self.winner = self.board_dict[1]
             return True
         elif self.board_dict[7] == self.board_dict[8] == self.board_dict[9] != Mark() or \
-             self.board_dict[3] == self.board_dict[6] == self.board_dict[9] != Mark():
+                self.board_dict[3] == self.board_dict[6] == self.board_dict[9] != Mark():
             self.winner = self.board_dict[9]
             return True
         else:
             return False
 
     def __str__(self):
-        board = str(self.board_dict[1]) + " | " + str(self.board_dict[2]) + "| " + str(self.board_dict[3]) + "\n" +\
-                "--------\n" +\
-                str(self.board_dict[4]) + " | " + str(self.board_dict[5]) + "| " + str(self.board_dict[6]) + "\n" +\
-                "--------\n" +\
+        board = str(self.board_dict[1]) + " | " + str(self.board_dict[2]) + "| " + str(self.board_dict[3]) + "\n" + \
+                "--------\n" + \
+                str(self.board_dict[4]) + " | " + str(self.board_dict[5]) + "| " + str(self.board_dict[6]) + "\n" + \
+                "--------\n" + \
                 str(self.board_dict[7]) + " | " + str(self.board_dict[8]) + "| " + str(self.board_dict[9])
         return board
-
